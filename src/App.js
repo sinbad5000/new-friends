ba6261cde6d6ddaac16dde9aafd7c34c829edc5a
sky777666 <lmyer12@my.wgu.edu>
2020-07-23 19:16:33 -0700

    Client-Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router,Route, Link} from 'react-router-dom'
 import './App.css';
+import Profile from './Profile'
+import Welcome from './Welcome'
+import Main from './Main'
+import Request from './Request'
+import Friends from './Friends'
 
 function App() {
+
+
+
+
+  const data = {
+          imgUrl: "http://placekitten.com/300/200", 
+          name: "This is a great website ! " ,
+          phone:"This is some content for the blog and we really like it. Please like or share us on Facebook and Instagram",
+          date: "July 17th, 2020",
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Welcome </Link>
+          <Link to="/Main">Main</Link>
+          <Link to="/Profile">Profile</Link>
+          <Link to="/Requests">Request</Link>
+          <Link to="/Friends">Friends</Link>
+        </nav>
+      </div>
+      <div className="App">
+        <Route exact path="/" component={Welcome} />
+        <Route path="/Main" component={Main} />
+        <Route path="/Profile" component={Profile} />
+        <Route path="/Main" render={() => <Main picture={data} />} />  
+        <Route path="/Requests" component={Request} /> 
+        <Route path="/Friends" component={Friends} />
+      </div>
+    </Router>
   );
 }
 
