62ee9b4498a14a4b85d61006f2fa39cba13402a1
Nick Foxen <foxen.nicholas@gmail.com>
2020-07-25 17:06:16 -0700

    added to User.js model

---
+++ back/models/Category.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const CategorySchema = new Schema({
+  
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Category = mongoose.model("categories", CategorySchema) 
