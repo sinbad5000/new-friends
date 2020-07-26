a2126df766159a72d0a7027f08019d329f8fe8b3
sinav <sinavameghi@gmail.com>
2020-07-26 10:38:53 -0700

    updated the auth

---
+++ front/src/content/components/FriendsCarousel.js
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+const FriendCarousel = () => {
+  return (
+    <div>
+      <h1>FriendCarousel</h1>
+    </div>
+  )
+}
+
+export default FriendCarousel;
---
+++ front/src/content/components/UserCard.js
@@ -0,0 +1,12 @@
+  
+import React from 'react';
+
+const UserCard = () => {
+  return (
+    <div>
+      <h1>UserCard</h1>
+    </div>
+  )
+}
+
+export default UserCard;
