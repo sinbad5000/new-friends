3b4ed5f8a8dc92d5532c8e66a8690896887d0087
sky777666 <lmyer12@my.wgu.edu>
2020-07-29 19:45:54 -0700

    Profile Code done

--- front/src/Main.js
+++ front/src/Main.js
@@ -5,8 +5,7 @@ import Axios from 'axios';
 // import { NavLink } from 'react-router-dom'
 // import Friends from './Friends' 
 // import Request from './content/components/Request'
-// import Profile from'./content/components/Profile'
-// import LikeProfile from './content/components/LikeProfile'
+import Profile from './content/components/Profile'
 
 
 const Main = (props) => {
@@ -36,33 +35,6 @@ const Main = (props) => {
       
           
       
-        // return (
-        //   <div className="bounty-card">
-        //     <h3>{props.name} is {props.captured ? captured : atLarge}</h3>
-        //     <h5>Wanted for {props.wantedFor}</h5>
-        //     <p>Bounty posted by: {props.client}</p>
-        //     <p>Reward: {props.reward}</p>
-        //     {props.lastSeen ? <p>{`Last seen ${props.lastSeen}`}</p> : null}
-        //     {props.ship ? <p>{`Currently using ${props.ship} as transportation`}</p> : null}
-      
-        //     <ul>
-        //       {hunterList}
-        //     </ul>
-        //     <DeleteButton id={props._id} refresh={props.refresh} setError={props.setError} />
-        //   </div>
-    //     )
-    //   }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -76,12 +48,7 @@ const Main = (props) => {
                 </div>
                     <div id="mainbox">
                     <h2> </h2>
-                    <img id="mainpic" src={`${props.blog.imgUrl}`} alt="face of a person" /> 
-                        <img id="logo" src="https://i.imgur.com/i6FmYyB.jpg"/>
-                        <h3>{props.blog.name}</h3>
-                    <p>{props.blog.phone}</p>
-                    <p>{props.blog.date}</p>  
-                </div>
+                    <Profile />
                 <div className="mainboxouter">
                     <img id="addbtn" src="https://i.imgur.com/iSV3icM.jpg" />
                 </div>
