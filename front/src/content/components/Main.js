ee5706992d5e3f090a9018c07b2b989a2705840f
sinav <sinavameghi@gmail.com>
2020-07-30 23:22:23 -0700

    this

--- front/src/content/components/Main.js
+++ front/src/content/components/Main.js
@@ -1,13 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-// import Button from 'react-bootstrap/Button';
-// import {BrowserRouter as Router,Route, Link} from 'react-router-dom'
-// import Navbar from './content/components/Navbar'
-// import { NavLink } from 'react-router-dom'
-// import Friends from './Friends' 
-// import Request from './content/components/Request'
-// import Profile from'./content/components/Profile'
-// import LikeProfile from './content/components/LikeProfile'
 import FriendCard from './FriendCard'
 
 
